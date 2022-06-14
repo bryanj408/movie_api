aa46fe12aaaad92069f510dc300279ffcefe32d0
Bryan Arndt <38339891+bryanj408@users.noreply.github.com>
2022-06-14 16:36:02 -0700

    REST API movie server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+//declares variables right away. require http, url, and fs(read file)
+const http = require('http'),
+  url = require('url'),
+  fs = require('fs');
+  //put in function "createServer once you figure it out"
+  //let addr = "http://localhost:8080/";
+
+http.createServer((request, response) => {
+  let addr = request.url,
+  q = url.parse(addr, true),
+  filePath = '';
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: '
+  + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, {'Content-Type': 'text/html'
+  });
+
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+
+console.log('My test server is running on port 8080.');
