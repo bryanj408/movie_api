be4c271c595bc07baa7e7c0df1ac2eeeca02b9a1
bryanj408 <bryan.arndt52@gmail.com>
2022-06-28 14:33:17 -0700

    updated index.js

--- index.js
+++ index.js
@@ -1,76 +1,239 @@
-const express = require('express');
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  morgan = require('morgan'),
+  uuid = require('uuid'),
+  fs = require('fs'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  Models = require('./models.js');
+  const cors = require('cors');
+
 const app = express();
-morgan = require('morgan');
-fs = require('fs');
-path = require('path');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let auth = require('./auth')(app);
+
+const passport = require('passport');
+require('./passport');
+
+const { check, validationResult } = require('express-validator');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', 
+{ useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesnt allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+/*
+app.post('/users', (req, res) => {
+  const newUser = req.body;
 
-let topMovies = [
-  {
-    title: "The Revenant",
-    director: "Alejandro González Iñárritu",
-    year: "2015"
+  if (newUser.name) {
+      newUser.id = uuid.v4();
+      users.push(newUser);
+      res.status(201).json(newUser);
+  } else {
+      res.status(400).send('Error somewhere');
+  }
+});
+*/
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+  .then((user) => {
+    if(user) {
+      return res.status(400).send(req.body.Username + 'already exists');
+    } else { 
+      Users
+        .create ({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        })
+        .then((user) => {res.status(201).json(user)
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  });
+});
+
+//POST method to add movie to favorite movie list
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $push: { FavoriteMovies: req.params.MovieID }
   },
-  {
-    title: "The Grand Budapest Hotel",
-    director: "Wes Anderson",
-    year: "2014"
-  },
-  {
-    title: "Saving Private Ryan",
-    director: "Steven Spielberg",
-    year: "1998"
-  },
-  {
-    title: "Gangs of New York",
-    director: "Martin Scorsese",
-    year: "2002"
-  },
-  {
-    title: "The Joker",
-    director: "Todd Phillips",
-    year: "2019"
-  },
-  {
-    title: "Inglourious Bastards",
-    director: "Quentin Tarantino",
-    year: "2009"
-  },
-  {
-    title: "There Will Be Blood",
-    director: "Paul Thomas Anderson",
-    year: "2007"
-  },
-  {
-    title: "They Shall Not Grow Old",
-    director: "Peter Jackson",
-    year: "2019"
-  },
-  {
-    title: "The Dark Knight",
-    director: "Christopher Nolan",
-    year: "2008"
-  },
-  {
-    title: "Darkest Hour",
-    director: "Joe Wright",
-    year: "2017"
-  },
-];
+  { new: true }, //This line makes sure that the updated document is returned
+  (err, updatedUser) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
 
 //Get requests
-app.send('/', (req, res) => {
-    res.send('Welcome to my movie list!')
+//Adding get request to get all users from 2.8 (ask teacher if changes are needed)
+//Get all users
+//READ Users - Get a user info
+app.get('/users', passport.authenticate('jwt', { session: false }), function (req, res) {
+  Users.find()
+    .then(function (users) {
+      res.status(200).json(users);
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//Get a user by username
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  });
+});
+//Adding get request to get all users from 2.8 (ask teacher if changes are needed)
+
+//PUT method to update user info
+app.put('/users/:Username', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username}, { $set: 
+  {
+    Username: req.body.Username,
+    Password: req.body.Password,
+    Email: req.body.Email,
+    Birthday: req.body.Birthday
+  }
+},
+{ new: true }, //this line makes sure that the updates docuent is returned
+  (err, updatedUser) => {
+    if(err) {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    } else {
+      res.json(updatedUser);
+    }
+  });
+});
+
+//Delete movie from user's favouriteMovies list
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate(
+    { Username: req.params.Username },
+    {
+      $pull: { FavoriteMovies: req.params.MovieID },
+    },
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    }
+  );
+});
+
+//DELETE user by username
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my myFlixDB!')
 });
 
 app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname })
 });
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies)
+//Display all movies
+app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+  Movies.find()
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error: " + err);
+  });
+});
+
+//READ Title-get- Get a movie by title
+app.get('/movies/:Title', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.findOne({ Title: req.params.Title })
+    .then((movie) => {
+      res.json(movie);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//READ Genre-get- Get movie genre info
+app.get('/movies/genre/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.findOne({ 'Genre.Name': req.params.Name })
+    .then((genre) => {
+      res.json(genre.Description);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+//READ Directors-get- Get a movie director
+app.get('/movies/director/:Name', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Movies.findOne({ 'Director.Name': req.params.Name })
+    .then((director) => {
+      res.json(director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 app.use((err, req, res, next) => {
