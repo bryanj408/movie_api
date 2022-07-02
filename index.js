40a8c3a50658a3d44fec202256f24b1b605524ea
bryanj408 <bryan.arndt52@gmail.com>
2022-07-02 14:55:57 -0700

    updated /users format

--- index.js
+++ index.js
@@ -40,48 +40,42 @@ app.use(cors({
   }
 }));
 
-app.post('/users', 
-  [
-    check('Username', 'Username is required.').isLength({min: 5}),
-    check('Username', 'Username contained non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required.').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid.').isEmail()
-  ], 
-    (req, res) => {
-
-  // check the validation object for erros
-    let errors = validationResults(req);
-
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array()
-      });
-    }
+//Create user
+app.post('/users',[
+  check('Username', 'Username is required').isLength({min: 5}),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+],(req,res)=>{
+  let errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
 
   let hashedPassword = Users.hashPassword(req.body.Password);
-  Users.findOne({ Username: req.body.Username })
-  .then((user) => {
-    if(user) {
-      return res.status(400).send(req.body.Username + 'already exists');
-    } else { 
+  Users.findOne({Username:req.body.Username})
+  .then(user=>{
+    if(user){
+      res.status(400).send(req.body.Username + ' already exists!')
+    }else{
       Users
-        .create ({
-          Username: req.body.Username,
-          Password: hashedPassword,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday
-        })
-        .then((user) => {res.status(201).json(user)
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        })
+      .create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then(user=> res.status(201).json(user))
+      .catch(error=>{
+        console.error(error);
+        res.status(500).send('Error: ' + error)
+      })
     }
   })
-  .catch((error) => {
+  .catch(error=>{
     console.error(error);
-    res.status(500).send('Error: ' + error);
-  });
+    res.status(500).send('Error: ' + error)
+  })
 });
 
 //POST method to add movie to favorite movie list
